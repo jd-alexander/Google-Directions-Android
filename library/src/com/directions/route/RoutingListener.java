382f23740bd3ffc3b2f02da0556204a4f9b77d43
Cyrille Berliat <cyrille@berliat.fr>
2013-11-12 23:58:37 +0100

    Refactoring Listener's callback name, setting up working example

--- library/src/com/directions/route/RoutingListener.java
+++ library/src/com/directions/route/RoutingListener.java
@@ -3,7 +3,7 @@ package com.directions.route;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 public interface RoutingListener {
-  public void onFailure();
-  public void onStart();
-  public void onSuccess(PolylineOptions mPolyOptions);
+  public void onRoutingFailure();
+  public void onRoutingStart();
+  public void onRoutingSuccess(PolylineOptions mPolyOptions);
 }
