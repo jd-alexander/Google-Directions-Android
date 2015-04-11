fd0288b2868f836972ef672457d82e43cbcb5296
Felipe Duarte <fcduarte@gmail.com>
2015-04-10 22:31:53 -0700

    Update Gradle version

--- example/src/com/directions/android/MyActivity.java
+++ example/src/com/directions/android/MyActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.directions.route.Route;
 import com.directions.route.Routing;
 import com.directions.route.RoutingListener;
 import com.google.android.gms.maps.CameraUpdate;
@@ -15,11 +16,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MyActivity extends FragmentActivity implements RoutingListener
-{
+public class MyActivity extends FragmentActivity implements RoutingListener {
     protected GoogleMap map;
     protected LatLng start;
     protected LatLng end;
+
     /**
      * This activity loads a map and then displays the route and pushpins on it.
      */
@@ -27,11 +28,11 @@ public class MyActivity extends FragmentActivity implements RoutingListener
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        SupportMapFragment fm = (SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         map = fm.getMap();
 
-        CameraUpdate center=CameraUpdateFactory.newLatLng(new LatLng(18.013610,-77.498803));
-        CameraUpdate zoom=  CameraUpdateFactory.zoomTo(15);
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(18.013610, -77.498803));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
 
         map.moveCamera(center);
         map.animateCamera(zoom);
@@ -47,32 +48,33 @@ public class MyActivity extends FragmentActivity implements RoutingListener
 
     @Override
     public void onRoutingFailure() {
-      // The Routing request failed
+        // The Routing request failed
     }
 
     @Override
     public void onRoutingStart() {
-      // The Routing Request starts
+        // The Routing Request starts
     }
 
     @Override
-    public void onRoutingSuccess(PolylineOptions mPolyOptions) {
-      PolylineOptions polyoptions = new PolylineOptions();
-      polyoptions.color(Color.BLUE);
-      polyoptions.width(10);
-      polyoptions.addAll(mPolyOptions.getPoints());
-      map.addPolyline(polyoptions);
+    public void onRoutingSuccess(PolylineOptions mPolyOptions, Route route) {
+        PolylineOptions polyOptions = new PolylineOptions();
+        polyOptions.color(Color.BLUE);
+        polyOptions.width(10);
+        polyOptions.addAll(mPolyOptions.getPoints());
+        map.addPolyline(polyOptions);
 
-      // Start marker
-      MarkerOptions options = new MarkerOptions();
-      options.position(start);
-      options.icon(BitmapDescriptorFactory.fromResource(R.drawable.start_blue));
-      map.addMarker(options);
+        // Start marker
+        MarkerOptions options = new MarkerOptions();
+        options.position(start);
+        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.start_blue));
+        map.addMarker(options);
 
-      // End marker
-      options = new MarkerOptions();
-      options.position(end);
-      options.icon(BitmapDescriptorFactory.fromResource(R.drawable.end_green));  
-      map.addMarker(options);
+        // End marker
+        options = new MarkerOptions();
+        options.position(end);
+        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.end_green));
+        map.addMarker(options);
     }
+
 }
